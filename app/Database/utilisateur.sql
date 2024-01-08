5b33aa3ef136c1296cd68dff173c194ca9b175fb
Clément PASQUET <E22A210W@ubuntu.u-inf-j-c114-07>
2024-01-08 09:14:02 +0100

    Rajout de la bd utilisateur

--- app/Database/utilisateur.sql
+++ app/Database/utilisateur.sql
@@ -1,4 +1,4 @@
-create table utilisateur(
+create table IF NOT EXISTS utilisateur(
                             mail varchar2(255) not null primary key,
                             password varchar2(255) not null,
                             status varchar2(255) default 'adherent' not null ,
@@ -6,3 +6,5 @@ create table utilisateur(
                             nom varchar2(64) not null,
                             prenom varchar2(64) not null
 );
+
+INSERT OR REPLACE INTO utilisateur (mail,password,status, username, nom, prenom) VALUES ('clement@pasquet.fr', 'password', 'online', 'screeny', 'clement', 'pasquet');
