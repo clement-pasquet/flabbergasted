28c8d0fe451a978cc394bd87752afc2151df14eb
Paul-Adrien Troeira <47333595+Apologieze@users.noreply.github.com>
2024-01-09 15:23:08 +0100

    Nouveau routeur global

--- app/Config/App.php
+++ app/Config/App.php
@@ -37,11 +37,11 @@ class App extends BaseConfig
      * Index File
      * --------------------------------------------------------------------------
      *
-     * Typically this will be your index.php file, unless you've renamed it to
+     * Typically this will be your accueil.php file, unless you've renamed it to
      * something else. If you are using mod_rewrite to remove the page set this
      * variable so that it is blank.
      */
-    public string $indexPage = 'index.php';
+    public string $indexPage = 'accueil.php';
 
     /**
      * --------------------------------------------------------------------------
