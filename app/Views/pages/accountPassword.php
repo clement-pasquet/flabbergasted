796b74c93011d0649810c40b41587a4c369e9749
E22A210W <clement.pasquet@etu.univ-nantes.fr>
2024-01-09 17:32:49 +0100

    Migration de accountPassword et accountPhone.

--- app/Views/pages/accountPassword.php
+++ app/Views/pages/accountPassword.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-  <title>accountPhone</title>
+  <link rel="stylesheet" href="<?= CSS.'accountPassword.css'?>">
+  <title>accountPassword</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
@@ -15,13 +15,15 @@
 
 <body>
   <div class="accountMainDiv">
-    <p class="accountPhoneTitle">Numéro</p>
-    <p class="accountNameSubtitle">Le numéro que vous indiquez ici sera associé à votre profil.<br>Ce numéro nous permettra de vérifier votre identité en cas de perte de mot de passe.</p>
+    <p class="accountTitle">Mot de passe</p>
+    <p class="accountSubtitle">Le mot de passe que vous indiquez ici sera associé à votre profil.<br>Mettez un mot de passe solide mais facilement mémorisable.</p>
 
-    <form action="../pages/accountInformation.php" method="post">
-      <p class="accountPhoneTitle">Numéro de téléphone</p>
-      <input id="ChamptexteName" type="tel" value="0123456789" placeholder="Entrez votre numéro de téléphone">
-      
+    <form action="#" method="post">
+      <p class="accountTitle">Modifier mot de passe</p>
+      <input id="ChamptextePassword" type="password" placeholder="Entrez votre nouveau mot de passe">
+      <br>
+      <input id="ChamptextePassword" type="password" placeholder="Confirmez votre nouveau mot de passe">
+
 
       <div class="buttonDiv">
         <button type="submit" class="updateButton">
--- app/Views/pages/accountPhone.php
+++ app/Views/pages/accountPhone.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-  <title>accountPassword</title>
+  <link rel="stylesheet" href="<?= CSS.'accountPhone.css'?>">
+  <title>accountPhone</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
@@ -15,15 +15,13 @@
 
 <body>
   <div class="accountMainDiv">
-    <p class="accountTitle">Mot de passe</p>
-    <p class="accountSubtitle">Le mot de passe que vous indiquez ici sera associé à votre profil.<br>Mettez un mot de passe solide mais facilement mémorisable.</p>
+    <p class="accountPhoneTitle">Numéro</p>
+    <p class="accountNameSubtitle">Le numéro que vous indiquez ici sera associé à votre profil.<br>Ce numéro nous permettra de vérifier votre identité en cas de perte de mot de passe.</p>
 
-    <form action="../pages/accountInformation.php" method="post">
-      <p class="accountTitle">Modifier mot de passe</p>
-      <input type="password" placeholder="Entrez votre nouveau mot de passe">
-      <br>
-      <input type="password" placeholder="Confirmez votre nouveau mot de passe">
-
+    <form action="#" method="post">
+      <p class="accountPhoneTitle">Numéro de téléphone</p>
+      <input id="ChamptexteName" type="tel" value="0123456789" placeholder="Entrez votre numéro de téléphone">
+      
 
       <div class="buttonDiv">
         <button type="submit" class="updateButton">
