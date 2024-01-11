75d40ec53928b0bdc14f034ef2fa3bee5bba478c
clement-pasquet <clementpasquet89100@laposte.net>
2024-01-11 18:29:29 +0100

    Ajout de achatModel.

---
+++ app/Models/achatModel.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class achatModel extends Model
+{
+    protected $table = 'achat';
+    protected $primaryKey = null; // adjust accordingly
+
+    // Crée un nouvelle achat grace a id_user et id_film
+    public function setAchat($id_user, $id_film)
+    {
+        $data = [
+            'id_user' => $id_user,
+            'id_film' => $id_film,
+        ];
+
+        // Insérer le nouvel achat dans la base de données
+        return $this->insert($data);
+
+    }
+
+}
