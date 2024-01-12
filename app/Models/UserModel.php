838ee895b02d561fecbd177ee7731e390c6f5428
Paul-Adrien Troeira <47333595+Apologieze@users.noreply.github.com>
2024-01-12 09:15:27 +0100

    Dump

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,64 +10,28 @@ class UserModel extends Model
     protected $primaryKey           = 'id_user';
     protected $insertID             = 0;
     protected $returnType           = 'array';
-    protected $useSoftDeletes       = false;
     protected $protectFields        = true;
     protected $allowedFields = ['mail', 'password', 'status', 'username', 'nom', 'prenom'];
 
-    // Donne l'utilisateur dont l'id est donné en paramètre ou tous les users si rien n'est donné
-    public function getUser($id_user = false)
-    {
-        if ($id_user === false) {
-            return $this->findAll();
-        }
 
-        return $this->where(['id_user' => $id_user])->first();
+    public function getUser($id_user)
+    {
+        return $this->find($id_user);
     }
 
-    public function setMail($id_user, $newEmail)
+    public function setEmail($id_user, $newEmail)
     {
         $data = [
-            'mail' => $newEmail,
+            'email' => $newEmail,
         ];
 
         return $this->update($id_user, $data);
     }
 
-    public function getUserByUsername($username)
+    public function getUserByUsernameOrEmail($usernameOrEmail)
     {
-        return $this->where('username', $username)
+        return $this->where('username', $usernameOrEmail)
+            ->orWhere('mail', $usernameOrEmail)
             ->first();
     }
-
-    // Setter pour modifier le nom d'un utilisateur
-    public function setNom($id_user, $nom)
-    {
-    $data = [
-        'nom'    => $nom,
-    ];
-
-    return $this->update($id_user, $data);
-    }
-
-    // Setter pour modifier le prenom d'un utilisateur
-    public function setPrenom($id_user, $prenom)
-    {
-    $data = [
-        'prenom' => $prenom,
-    ];
-
-    return $this->update($id_user, $data);
-    }
-
-
-    // Setter pour modifier le prenom d'un utilisateur
-    public function setPassword($id_user, $password)
-    {
-        $data = [
-            'password' => $password,
-        ];
-
-        return $this->update($id_user, $data);
-    }
-
 }
