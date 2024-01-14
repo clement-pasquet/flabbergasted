c4ecc592265841adf0138d3041ac4385646a53cd
clement-pasquet <clementpasquet89100@laposte.net>
2024-01-14 20:11:24 +0100

    Correction push précédent

--- app/Views/pages/resultSearch.php
+++ app/Views/pages/resultSearch.php
@@ -3,26 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--  appel du header-->
+    <?php include('header.php'); ?>
     <link rel="stylesheet" href="<?=CSS.'searchStyle.css'?>">
     <title>Resultat de la recherche:</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
 </head>
 <body>
-<div class="cardsFlex">
-<?php if (isset($films)): ?>
-<?php foreach ($films as $film): ?>
 
-    <a href="../filmFocused/<?=$film['id_film']?>" class="link_style"><p class="titleFilm"><?= $film['titre']?></p>
-        <div class="cardFlex">
-        <img class="cardPic" src="<?=IMG.$film['image']?>" alt="<?=$film['titre']?>">
-        <p>Genre : <?= $film['genre']?></p>
-        <p>NB likes : <?= $film['likes']?></p>
-        <p>Réalisateur : <?= $film['realisateur'] ?> </p>
-        </div>
-    </a>
 
-<?php endforeach; ?>
-<?php endif; ?>
+
+<div class="contentDiv">
+    <?php if (isset($films)): ?>
+        <?php foreach ($films as $film): ?>
+            <a href="../filmFocused/<?=$film['id_film']?>" class="link_style">
+                <div class="cardDiv">
+                    <img class="cardPic" src="<?=IMG.$film['image']?>" alt="<?=$film['titre']?>">
+                    <div class="cardInfo">
+                        <p class="titleFilm"><?= $film['titre']?></p>
+                        <div class="cardInfoWithoutTitle">
+                            <p>Genre : <?= $film['genre']?></p>
+                            <p>Likes : <?= $film['likes']?></p>
+                            <p>Réalisateur(s) : <?= $film['realisateur'] ?> </p>
+                            <p>Date de sortie : <?= $film['date_release'] ?></p>
+                            <p>Pays : <?= $film['pays'] ?></p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
+
+
+
+
+
 </body>
 </html>
